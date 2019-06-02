3358841c92be9856999a44a88250c984cf2a1908
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-06-02 14:30:15 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import couple from "./images/K18.png";
+
+function App() {
+  return (
+    <div className="vh-100 flex flex-column justify-center items-center">
+      <img src={couple} style={{ width: "60%" }} />
+      <div>Wedding Day</div>
+    </div>
+  );
+}
+
+export default App;
