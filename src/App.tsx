f71e1d3751cf3f99a245c3abf7b295a783c84ef1
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-06-02 15:15:25 +0700

    positioning

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,43 @@ import React from "react";
 import "./App.css";
 import couple from "./images/couple.png";
 import separator from "./images/separator.png";
+import topLeft from "./images/top-left.png";
 
 function App() {
   return (
-    <div className="vh-100 flex flex-column justify-center items-center">
-      <img src={couple} style={{ width: "60%" }} />
-      <div>Wedding Day</div>
-      <div>@mikqi &amp; @ichafizha</div>
+    <div className="vh-100 flex flex-column justify-center items-center sinhala">
+      <img src={topLeft} className="absolute w-30 top-0 left-0" />
+
+      {/* IMAGE */}
+      <img src={couple} className="mw-70 w-70" />
+
+      {/* TITLE */}
+      <div className="autumn mt3 f2">Wedding Day</div>
+
+      {/* COUPLE */}
+      <div className="artilla mb3 mt2 f3">@mikqi &amp; @ichafizha</div>
+
+      {/* SEPARATOR */}
       <img src={separator} style={{ width: "20%" }} />
-      <div>
-        <div>06 Juli 2019</div>
-        <div>Akad: 09.00 &amp; Resepsi: 11.00 - 13.00</div>
+
+      {/* TIME */}
+      <div className="mt3 flex justify-center items-center flex-column">
+        <div className="f3">06 Juli 2019</div>
       </div>
-      <div>Ixora Hall</div>
-      <div>
+      <div className="flex gray">
+        <div className="f4 flex flex-column items-center mr3 lh-solid">
+          <span>Akad Nikah</span>
+          <span>09.00 - Selesai</span>
+        </div>
+        <div className="f4 flex flex-column items-center ml3 lh-solid">
+          <span>Resepsi</span>
+          <span>11.00 - 13.00</span>
+        </div>
+      </div>
+
+      {/* LOCATION */}
+      <div className="mt3 f3">Ixora Hall</div>
+      <div className="f4 w-70 tc gray">
         Jl. Imam Bonjol No. 32, Panunggangan Bar, Kec. Karawaci, Kota Tangerang,
         Banten 15115
       </div>
