9a3a3200a951bf23008f183c2e7565c23357c22e
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-06-02 16:52:14 +0700

    add scroll indicator

--- src/App.tsx
+++ src/App.tsx
@@ -17,39 +17,49 @@ function App() {
     >
       <img src={topLeft} className="absolute w-30 top-0 left-0" />
       <img src={topRight} className="absolute w-30 top-0 right-0" />
-      <div className="vh-100 flex flex-column justify-center items-center sinhala">
-        {/* IMAGE */}
-        <img src={couple} className="z-1 mw-70 w-70" />
+      <div className="vh-100 flex flex-column justify-between items-center sinhala">
+        <div className="flex flex-column flex-auto justify-center items-center sinhala">
+          {/* IMAGE */}
+          <img src={couple} className="z-1 mw-70 w-70" />
 
-        {/* TITLE */}
-        <div className="z-1 autumn mt3 f2">Wedding Day</div>
+          {/* TITLE */}
+          <div className="z-1 autumn mt3 f2">Wedding Day</div>
 
-        {/* COUPLE */}
-        <div className="z-1 artilla mb3 mt2 f3">@mikqi &amp; @ichafizha</div>
+          {/* COUPLE */}
+          <div className="z-1 artilla mb3 mt2 f3">@mikqi &amp; @ichafizha</div>
 
-        {/* SEPARATOR */}
-        <img src={separator} style={{ width: "20%" }} />
+          {/* SEPARATOR */}
+          <img src={separator} style={{ width: "20%" }} />
 
-        {/* TIME */}
-        <div className="z-1 mt3 flex justify-center items-center flex-column">
-          <div className="z-1 f3">06 Juli 2019</div>
-        </div>
-        <div className="z-1 flex gray">
-          <div className="z-1 f4 flex flex-column items-center mr3 lh-solid">
-            <span>Akad Nikah</span>
-            <span>09.00 - Selesai</span>
+          {/* TIME */}
+          <div className="z-1 mt3 flex justify-center items-center flex-column">
+            <div className="z-1 f3">06 Juli 2019</div>
           </div>
-          <div className="z-1 f4 flex flex-column items-center ml3 lh-solid">
-            <span>Resepsi</span>
-            <span>11.00 - 13.00</span>
+          <div className="z-1 flex gray">
+            <div className="z-1 f4 flex flex-column items-center mr3 lh-solid">
+              <span>Akad Nikah</span>
+              <span>09.00 - Selesai</span>
+            </div>
+            <div className="z-1 f4 flex flex-column items-center ml3 lh-solid">
+              <span>Resepsi</span>
+              <span>11.00 - 13.00</span>
+            </div>
+          </div>
+
+          {/* LOCATION */}
+          <div className="z-1 mt3 f3">Ixora Hall</div>
+          <div className="z-1 f4 w-70 tc gray">
+            Jl. Imam Bonjol No. 32, Panunggangan Bar, Kec. Karawaci, Kota
+            Tangerang, Banten 15115
           </div>
         </div>
 
-        {/* LOCATION */}
-        <div className="z-1 mt3 f3">Ixora Hall</div>
-        <div className="z-1 f4 w-70 tc gray">
-          Jl. Imam Bonjol No. 32, Panunggangan Bar, Kec. Karawaci, Kota
-          Tangerang, Banten 15115
+        <div className="scroll-indicator">
+          <img
+            src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBkPSJNMTYuMDAzIDE4LjYyNmw3LjA4MS03LjA4MUwyNSAxMy40NmwtOC45OTcgOC45OTgtOS4wMDMtOSAxLjkxNy0xLjkxNnoiLz48L3N2Zz4="
+            width="50"
+            style={{ opacity: 0.3 }}
+          />
         </div>
       </div>
       <img src={bottom} className="w-100 mt6" />
