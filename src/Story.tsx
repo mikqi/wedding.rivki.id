37cae770cfe5b25269127ab4290fdab14c6a0830
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-06-17 01:08:10 +0700

    add arrow background

--- src/Story.tsx
+++ src/Story.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
-function Story({image, paragraph}) {
+function Story({image, paragraph, isFontSpecial}) {
   return (
     <div className="flex flex-auto mt5 w-90 justify-center items-center">
       <img className="flex flex-auto" style={{width: 200}} src={require(`./images/stories/l${image}.jpg`)} />
-      <div className="tc">{paragraph} + {image}</div>
+      <div
+        className={`${isFontSpecial ? 'artilla' : ''} tc relative`}
+      >
+        <div
+          style={{
+            background: `url(${require(`./images/arrows/arr${Math.ceil(Math.random() * 4)}.png`)}) no-repeat`,
+            backgroundSize: 'contain',
+            backgroundPosition: 'center',
+            width: 100,
+            height: 150,
+            top: '-150%',
+            right: `${Math.ceil(Math.random() * 25)}%`,
+            position: 'absolute',
+            opacity: .5
+          }}
+        />
+        {paragraph}
+      </div>
       <img className="flex flex-auto" style={{width: 200}} src={require(`./images/stories/p${image}.jpg`)} />
     </div>
   )
